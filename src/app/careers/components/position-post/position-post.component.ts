e2fd0f13f013559afa4d5a3a7d671ba4c9cb6970
Jose Ricardo Osorio Soto <osorioj842@gmail.com>
2021-06-26 21:47:14 +0000

    Positions posts component template turned dynamic

--- src/app/careers/components/position-post/position-post.component.ts
+++ src/app/careers/components/position-post/position-post.component.ts
@@ -6,6 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./position-post.component.scss']
 })
 export class PositionPostComponent implements OnInit {
+  posts: Array<any> = [
+    {
+      title: 'Inbound Customer Service Associate ',
+      company: '@Boost BPO',
+      address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
+      modality: 'Full Time',
+      date: '25 FEB, 2020',
+    },
+    {
+      title: 'Call Examiner - Health Care',
+      company: '@Boost BPO',
+      address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
+      modality: 'Full Time',
+      date: '25 FEB, 2020',
+    },
+    {
+      title: 'Lead Profiler',
+      company: '@Boost BPO',
+      address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
+      modality: 'Full Time',
+      date: '25 FEB, 2020',
+    },
+    {
+      title: 'Virtual Assistant',
+      company: '@Boost BPO',
+      address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
+      modality: 'Full Time',
+      date: '25 FEB, 2020',
+    }
+  ];
 
   constructor() { }
 
