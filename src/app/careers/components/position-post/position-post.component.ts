15b7019ac898fddc537cc87f24671e0f1662a1d3
Jose Ricardo Osorio Soto <osorioj842@gmail.com>
2021-07-13 20:24:42 -0400

    UI design hirearchy principle applied to the careers position cards. Posts model created in the careers/models package.

--- src/app/careers/components/position-post/position-post.component.ts
+++ src/app/careers/components/position-post/position-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DEFAULT_POSTS } from '../../models/posts.model';
 
 
 @Component({
@@ -10,36 +11,7 @@ export class PositionPostComponent implements OnInit {
   @Input() savedPosts: Array<any> = [];
   @Output() cardEvent: EventEmitter<any> = new EventEmitter<any>();
 
-  posts: Array<any> = [
-    {
-      title: 'Inbound Customer Service Associate ',
-      company: '@Boost BPO',
-      address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
-      modality: 'Full Time',
-      date: '25 FEB, 2020',
-    },
-    {
-      title: 'Call Examiner - Health Care',
-      company: '@Boost BPO',
-      address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
-      modality: 'Full Time',
-      date: '25 FEB, 2020',
-    },
-    {
-      title: 'Lead Profiler',
-      company: '@Boost BPO',
-      address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
-      modality: 'Full Time',
-      date: '25 FEB, 2020',
-    },
-    {
-      title: 'Virtual Assistant',
-      company: '@Boost BPO',
-      address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
-      modality: 'Full Time',
-      date: '25 FEB, 2020',
-    }
-  ];
+  posts: Array<any> = DEFAULT_POSTS;
 
   constructor() { }
 
---
+++ src/app/careers/models/posts.model.ts
@@ -0,0 +1,43 @@
+export interface Post {
+    title: string; 
+    company: string;
+    address: string;
+    sector: string;
+    modality: string;
+    date: string;
+}
+
+export const DEFAULT_POSTS: Array<Post> = [
+    {
+        title: 'Inbound Customer Service Associate ',
+        company: '@Boost BPO',
+        address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
+        sector: 'business services',
+        modality: 'Full Time',
+        date: '25 FEB, 2020',
+      },
+      {
+        title: 'Call Examiner - Health Care',
+        company: '@Boost BPO',
+        address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
+        sector: 'business services',
+        modality: 'Full Time',
+        date: '25 FEB, 2020',
+      },
+      {
+        title: 'Lead Profiler',
+        company: '@Boost BPO',
+        address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
+        sector: 'business services',
+        modality: 'Full Time',
+        date: '25 FEB, 2020',
+      },
+      {
+        title: 'Virtual Assistant',
+        company: '@Boost BPO',
+        address: 'Avenida Carlos Perez Ricart 35, Santo Domingo, Dominican Republic',
+        sector: 'business services',
+        modality: 'Full Time',
+        date: '25 FEB, 2020',
+      }
+];
